65967d58f2c3eaaa749ec299be7b1c5098f8c18c
Vitor Oliveira <vitorroliveiraa@outlook.com.br>
2022-07-19 17:10:41 -0300

    refactor: adding login on connection to db

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -3,9 +3,22 @@ import { app } from './app';
 
 const PORT = process.env.PORT || 3000;
 
-createConnection('localhost');
+createConnection('localhost')
+  .then(() => {
+    console.log('------------------------------------------');
+    console.log('💡 Data Source has been initialized!');
+    console.log('------------------------------------------');
+  })
+  .catch(error => {
+    console.log('------------------------------------------');
+    console.error('💥 Error during Data Source initialization =>', {
+      ...error,
+    });
+    console.log('------------------------------------------');
+  });
 
 app.listen(PORT, () => {
+  console.log('------------------------------------------');
   console.log(`🚀 Server started on http://localhost:${PORT}`);
   console.log('------------------------------------------');
 });
